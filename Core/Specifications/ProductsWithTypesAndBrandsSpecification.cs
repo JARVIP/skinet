124d674c1e398226cd261820566518171f84bee5
oleg gugunava <oleggugunava@gmail.com>
2021-03-18 17:14:05 +0400

    Add Filtering paging and Cors policy

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -4,10 +4,33 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
+            :base(x=>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
         {
             AddInclude(x=>x.ProductBrand);
             AddInclude(x=>x.ProductType);
+
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1), productParams.PageSize);
+
+            if(!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch(productParams.Sort)
+                {
+                    case "PriceAsc":
+                        AddOrderBy(x=>x.Price);
+                        break;
+                    case "PriceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default: 
+                        AddOrderBy(x=>x.Name);
+                        break;
+                }
+            }
         }
         
         public ProductsWithTypesAndBrandsSpecification(int id)
