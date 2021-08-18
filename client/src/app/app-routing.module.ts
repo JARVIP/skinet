4bc0997d46a613cd0034adcbfc2f3933dd70ea7e
Oleg Gugunava <oleggugunava@gmail.com>
2021-08-18 22:57:55 +0400

    ch

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,19 +7,20 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: {breadcrumb: 'Home'} },
-  { path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'} },
-  { path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'} },
-  { path: 'not-found', component: NotFoundComponent , data: {breadcrumb: 'Nog Found'}},
-  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'shop'}},
-  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'basket'}},
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+  { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Errors' } },
+  { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
+  { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Nog Found' } },
+  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'shop' } },
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: { breadcrumb: 'basket' } },
   {
     path: 'checkout',
     canActivate: [AuthGuard],
     loadChildren: () => import('./checkout/checkout.module')
-      .then(mod => mod.CheckoutModule), data: {breadcrumb: 'checkout'}
+      .then(mod => mod.CheckoutModule), data: { breadcrumb: 'checkout' }
   },
-  { path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule), data: {breadcrumb: {skip: true}}},
+  { path: 'orders', loadChildren: () => import('./orders/orders.module').then(mod => mod.OrdersModule), data: { breadcrumb: 'Orders' } },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule), data: { breadcrumb: { skip: true } } },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
